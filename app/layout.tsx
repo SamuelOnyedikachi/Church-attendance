40b8d3675c59e7a8b5eeddd8945e5efce696b897
Samuel Onyedikachi <samuelonyedikachi450@gmail.com>
2026-02-20 16:58:49 +0100

    updated metadata title

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import 'react-toastify/dist/ReactToastify.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Church Attendance',
+  title: 'RYTHMN 5FELLOWSHIP UGH',
   description: 'Church Attendance Management',
 };
 
